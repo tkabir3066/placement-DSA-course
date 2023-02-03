dda011e96ede651bb664538e3b7bb1b75f000ec3
Tutul Kabir <tutulkabir@gmail.com>
2023-02-03 22:38:51 +0530

    initial set up and updated lecture-3

---
+++ Lecture-3/conditionalsAndLoops.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+
+
+
+
+int main(){
+
+/* int n;
+cout<< "Enter an integer: ";
+cin>>n;
+ cout<<"Value of n is :"<<n; */
+ 
+
+ /* int a;
+ cout<<"Enter an integer: ";
+ cin>>a;
+
+   // if a is positive or negative
+
+   if(a>0){
+    cout<< "A is positive"<<endl;
+   }else if(a<0){
+    cout<<"A is negative "<<endl;
+   }else{
+    cout<<"Zero";
+   }
+ */
+
+ int a, b;
+
+cout<<"Enter two number : ";
+ cin>>a>>b;
+
+ cout<< "value of a and b = "<<a<<" "<<b;
+
+
+    getch();
+}
